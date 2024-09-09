23aa78ab47e8ca9aeabef1bdb388f20ef0e31bbd
박동현 <jinee9523@gmail.com>
2024-09-09 15:59:15 +0900

    Comment: 오류 임시 주석 처리

--- total-repo/server/src/main/java/com/d108/project/interfaces/api/favorate/FavoriteFranchiseApi.java
+++ total-repo/server/src/main/java/com/d108/project/interfaces/api/favorate/FavoriteFranchiseApi.java
@@ -1,7 +1,6 @@
 package com.d108.project.interfaces.api.favorate;
 
-import com.d108.project.domain.favorite.favoriteFranchise.FavoriteFranchiseRequestDto;
-import com.d108.project.domain.favorite.favoriteFranchise.FavoriteFranchiseResponseDto;
+
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +10,12 @@ import java.util.List;
 @RequestMapping("/favorite/franchises")
 public interface FavoriteFranchiseApi {
 
-    @GetMapping("/list")
-    ResponseEntity<List<FavoriteFranchiseResponseDto>> getFavoriteFranchisesByMember(@RequestParam Long memberId);
-
-    @DeleteMapping("{favoriteFranchiseId}")
-    ResponseEntity<Object> deleteFavoriteFranchise(@RequestParam Long memberId, @PathVariable Long favoriteFranchiseId);
-
-    @PostMapping
-    ResponseEntity<Object> createFavoriteFranchise(@RequestParam Long memberId, @RequestBody FavoriteFranchiseRequestDto favoriteFranchiseRequestDto);
+//    @GetMapping("/list")
+//    ResponseEntity<List<FavoriteFranchiseResponseDto>> getFavoriteFranchisesByMember(@RequestParam Long memberId);
+//
+//    @DeleteMapping("{favoriteFranchiseId}")
+//    ResponseEntity<Object> deleteFavoriteFranchise(@RequestParam Long memberId, @PathVariable Long favoriteFranchiseId);
+//
+//    @PostMapping
+//    ResponseEntity<Object> createFavoriteFranchise(@RequestParam Long memberId, @RequestBody FavoriteFranchiseRequestDto favoriteFranchiseRequestDto);
 }
