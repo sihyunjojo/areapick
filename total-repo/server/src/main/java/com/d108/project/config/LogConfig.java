476c3e95b7e4133442b5e1849b5e847d2279b5dc
조시현 <si4018@naver.com>
2024-09-10 13:26:02 +0900

    feat: yml 파일 private 관리하기 위한 코드 추가

---
+++ total-repo/server/src/main/java/com/d108/project/config/LogConfig.java
@@ -0,0 +1,16 @@
+package com.d108.project.config;
+
+public class LogConfig {
+    public static void configureLog4j() {
+        // 환경 변수 LOG_CONFIG를 통해 log4j 설정 파일 경로를 설정
+        String logConfigFile = System.getenv("LOG_CONFIG");
+
+        // log4j 설정 파일을 시스템 속성으로 지정
+        if (logConfigFile != null) {
+            System.setProperty("log4j.configurationFile", logConfigFile);
+        } else {
+            System.err.println("LOG_CONFIG 환경 변수가 설정되지 않았습니다.");
+            throw new IllegalStateException("LOG_CONFIG 환경 변수가 설정되지 않았습니다.");
+        }
+    }
+}
