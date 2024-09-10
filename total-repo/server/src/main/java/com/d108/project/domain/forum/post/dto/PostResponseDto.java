17b45219dc7c53c7d812f8e90ee3afec39204dd7
박동현 <jinee9523@gmail.com>
2024-09-10 09:22:46 +0900

    Feat: 생성 및 수정일 추가

--- total-repo/server/src/main/java/com/d108/project/domain/forum/post/dto/PostResponseDto.java
+++ total-repo/server/src/main/java/com/d108/project/domain/forum/post/dto/PostResponseDto.java
@@ -1,8 +1,13 @@
 package com.d108.project.domain.forum.post.dto;
 
 import com.d108.project.domain.forum.post.entity.Post;
+import com.d108.project.domain.forum.reply.dto.ReplyResponseDto;
 import lombok.Builder;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder
 @Setter
@@ -12,8 +17,14 @@ public class PostResponseDto {
     private String title;
     private String content;
     private Long view;
+
     // TODO: 댓글 안이어줬네..이어주기
+    private List<ReplyResponseDto> reply;
+
     // TODO: 글 전체적으로 생성일 수정일 같은거 출력 안하니까 이것도 같이 dto에 넣어줘야함
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
     public static PostResponseDto from(Post post) {
         return PostResponseDto.builder()
                 .id(post.getId())
@@ -21,6 +32,11 @@ public class PostResponseDto {
                 .title(post.getTitle())
                 .content(post.getContent())
                 .view(post.getView())
+                .createdAt(post.getCreatedAt())
+                .updatedAt(post.getUpdatedAt())
+                .reply(post.getReplies().stream()
+                        .map(ReplyResponseDto::from)
+                        .collect(Collectors.toList()))
                 .build();
     }
 }
--- total-repo/server/src/main/java/com/d108/project/domain/forum/reply/dto/ReplyByPostIdResponseDto.java
+++ total-repo/server/src/main/java/com/d108/project/domain/forum/reply/dto/ReplyByPostIdResponseDto.java
@@ -3,15 +3,21 @@ package com.d108.project.domain.forum.reply.dto;
 import com.d108.project.domain.forum.reply.entity.Reply;
 import lombok.Builder;
 
+import java.time.LocalDateTime;
+
 @Builder
 public class ReplyByPostIdResponseDto {
     private Long memberId;
     private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public static ReplyByPostIdResponseDto from(Reply reply) {
         return ReplyByPostIdResponseDto.builder()
                 .memberId(reply.getMember().getId())
                 .content(reply.getContent())
+                .createdAt(reply.getCreatedAt())
+                .updatedAt(reply.getUpdatedAt())
                 .build();
     }
 }
