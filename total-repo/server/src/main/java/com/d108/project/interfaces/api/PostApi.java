037f380d6fa4569176876b1c95570ca08d6a5f43
박동현 <jinee9523@gmail.com>
2024-09-10 09:27:00 +0900

    Refactor: 댓글-게시글 컨트롤러 분리

--- total-repo/server/src/main/java/com/d108/project/interfaces/api/PostApi.java
+++ total-repo/server/src/main/java/com/d108/project/interfaces/api/PostApi.java
@@ -1,18 +1,13 @@
 package com.d108.project.interfaces.api;
 
 import com.d108.project.domain.forum.post.dto.PostCreateDto;
-import com.d108.project.domain.forum.post.dto.PostDeleteDto;
 import com.d108.project.domain.forum.post.dto.PostResponseDto;
 import com.d108.project.domain.forum.post.dto.PostUpdateDto;
-import com.d108.project.domain.forum.reply.dto.ReplyByPostIdResponseDto;
-import com.d108.project.domain.forum.reply.dto.ReplyCreateDto;
-import com.d108.project.domain.forum.reply.dto.ReplyUpdateDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-// TODO: 얘네 분리해야겠지..? 근데 어케 함
 @RequestMapping("/posts")
 public interface PostApi {
 
@@ -30,16 +25,4 @@ public interface PostApi {
 
     @DeleteMapping("/{postId}")
     ResponseEntity<Void> deletePost(@PathVariable("postId") Long postId, @RequestParam Long memberId);
-
-    @PostMapping("/{postId}/replies")
-    ResponseEntity<Void> createReply(@PathVariable("postId") Long postId, @RequestBody ReplyCreateDto replyCreateDto);
-
-    @GetMapping("/{postId}/replies")
-    ResponseEntity<List<ReplyByPostIdResponseDto>> getAllReplyByPostId(@PathVariable("postId") Long postId);
-
-    @PutMapping("/{postId}/replies/{replyId}")
-    ResponseEntity<Void> updateReply(@PathVariable("postId") Long postId, @PathVariable("replyId") Long replyId, @RequestParam Long memberId, @RequestBody ReplyUpdateDto replyUpdateDto);
-
-    @DeleteMapping("/{postId}/replies/{replyId}")
-    ResponseEntity<Void> deleteReply(@PathVariable("postId") Long postId, @PathVariable("replyId") Long replyId, @RequestParam Long memberId);
 }
--- total-repo/server/src/main/java/com/d108/project/interfaces/controller/PostController.java
+++ total-repo/server/src/main/java/com/d108/project/interfaces/controller/PostController.java
@@ -2,13 +2,8 @@ package com.d108.project.interfaces.controller;
 
 import com.d108.project.domain.forum.post.service.PostService;
 import com.d108.project.domain.forum.post.dto.PostCreateDto;
-import com.d108.project.domain.forum.post.dto.PostDeleteDto;
 import com.d108.project.domain.forum.post.dto.PostResponseDto;
 import com.d108.project.domain.forum.post.dto.PostUpdateDto;
-import com.d108.project.domain.forum.reply.ReplyService;
-import com.d108.project.domain.forum.reply.dto.ReplyByPostIdResponseDto;
-import com.d108.project.domain.forum.reply.dto.ReplyCreateDto;
-import com.d108.project.domain.forum.reply.dto.ReplyUpdateDto;
 import com.d108.project.interfaces.api.PostApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +18,6 @@ import java.util.List;
 public class PostController implements PostApi {
 
     private final PostService postService;
-    private final ReplyService replyService;
 
     @Override
     public ResponseEntity<Void> createPost(PostCreateDto postCreateDto) {
@@ -60,26 +54,4 @@ public class PostController implements PostApi {
         return ResponseEntity.noContent().build();
     }
 
-    @Override
-    public ResponseEntity<Void> createReply(Long postId, ReplyCreateDto replyCreateDto) {
-        replyService.createReply(postId, replyCreateDto);
-        return ResponseEntity.ok().build();
-    }
-
-    @Override
-    public ResponseEntity<List<ReplyByPostIdResponseDto>> getAllReplyByPostId(Long postId) {
-        return ResponseEntity.ok(replyService.getAllReplyByPostId(postId));
-    }
-
-    @Override
-    public ResponseEntity<Void> updateReply(Long postId, Long replyId, Long memberId, ReplyUpdateDto replyUpdateDto) {
-        replyService.updateReply(replyId, memberId, replyUpdateDto);
-        return ResponseEntity.ok().build();
-    }
-
-    @Override
-    public ResponseEntity<Void> deleteReply(Long postId, Long replyId, Long memberId) {
-        replyService.deleteReply(replyId, memberId);
-        return ResponseEntity.noContent().build();
-    }
 }
