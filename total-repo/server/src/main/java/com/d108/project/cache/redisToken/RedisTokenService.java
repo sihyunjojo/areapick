251c072362253268342710fe39ee36b302f5c387
박동현 <jinee9523@gmail.com>
2024-09-11 14:02:04 +0900

    Refactor: RedisTokenService의 기능 JwtUtil에 병합

--- total-repo/server/src/main/java/com/d108/project/cache/redisToken/RedisTokenService.java
+++
@@ -1,7 +0,0 @@
-package com.d108.project.cache.redisToken;
-
-import com.d108.project.cache.redisToken.dto.TokenResponseDto;
-
-public interface RedisTokenService {
-    TokenResponseDto getToken(String username);
-}
--- total-repo/server/src/main/java/com/d108/project/config/security/util/JwtUtil.java
+++ total-repo/server/src/main/java/com/d108/project/config/security/util/JwtUtil.java
@@ -1,15 +1,26 @@
 package com.d108.project.config.security.util;
 
+import com.d108.project.cache.redis.RedisUtil;
+import com.d108.project.cache.redisToken.dto.TokenResponseDto;
+import com.d108.project.domain.loginCredential.entity.LoginCredential;
+import com.d108.project.domain.loginCredential.repository.LoginCredentialRepository;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
+@RequiredArgsConstructor
 @Component
 public class JwtUtil {
 
@@ -17,8 +28,11 @@ public class JwtUtil {
     public Long ACCESS_TOKEN_EXPIRE;
     @Value("${spring.jwt.refresh-token.expire-time}")
     public Long REFRESH_TOKEN_EXPIRE;
-
+    private final RedisUtil redisUtil;
+    private final LoginCredentialRepository loginCredentialRepository;
     private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
+    private static final String REDIS_ACCESS_TOKEN_PREFIX = "auth:accessToken:";
+
 
     // JWT 서명에 사용할 비밀키 (application.properties 또는 application.yml에서 주입됨)
     @Value("${spring.jwt.salt}")
@@ -30,10 +44,12 @@ public class JwtUtil {
 //        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
 //        return Keys.hmacShaKeyFor(keyBytes);
 //    }
+
+    // 키 생성 방식 변경
     private Key getSigningKey() {
-        // 키 생성 방식 변경
-        return Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
     }
+
     // 주어진 Member 객체를 기반으로 JWT 토큰을 생성하는 메서드
     // 토큰에는 사용자의 사용자 이름(subject), 발행 시간, 만료 시간이 포함됨
     public String generateToken(String username, Long expireTime) {
@@ -75,5 +91,72 @@ public class JwtUtil {
                 .parseClaimsJws(token) // JWT의 클레임을 파싱
                 .getBody() // JWT의 body 부분(클레임)을 가져옴
                 .getSubject(); // 클레임에서 subject(사용자 이름)를 반환
+    }
+
+    public String extractToken(
+            @NonNull HttpServletRequest request,
+            String tokenType
+    ) {
+        Cookie[] cookies = request.getCookies();
+        String token = null;
+        // 쿠키에서 토큰 추출
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(tokenType)) {
+                    token = cookie.getValue();
+                    return token;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void deleteCookie(
+            @NonNull HttpServletResponse response
+    ) {
+        Cookie cookieRefresh = new Cookie("refresh_token", "");
+        cookieRefresh.setMaxAge(0);
+        cookieRefresh.setPath("/");
+        response.addCookie(cookieRefresh);
+
+        Cookie cookieAccess = new Cookie("refresh_token", "");
+        cookieAccess.setMaxAge(0);
+        cookieAccess.setPath("/");
+        response.addCookie(cookieAccess);
+    }
+
+    public void tokenRefresh(
+            @NonNull HttpServletResponse response,
+            String refreshToken) throws RuntimeException {
+        // 리프레시 토큰에서 추출한 username을 통해 DB에 저장된 리프레시 토큰을 비교
+        String storedToken = loginCredentialRepository.findRefreshTokenByUsername(refreshToken)
+                .orElse(null);
+
+        // 근데 토큰이 DB에 저장된 것과 다른 경우
+        if (!refreshToken.equals(storedToken)) {
+            deleteCookie(response);
+            throw new RuntimeException("토큰이 유효하지 않습니다.");
+        }
+
+        // 리프레시 토큰이 확인 되었으면
+        // 리프레시 토큰을 통해 새로 토큰을 발급
+        String username = getUsernameFromToken(refreshToken);
+        getToken(username);
+    }
+
+    public TokenResponseDto getToken(String username) {
+
+        String accessToken = generateToken(username, 1000 * ACCESS_TOKEN_EXPIRE);
+        String refreshToken = generateToken(username, 1000 * REFRESH_TOKEN_EXPIRE);
+
+        // accessToken은 레디스에
+        redisUtil.setDataExpire(REDIS_ACCESS_TOKEN_PREFIX + username, accessToken, ACCESS_TOKEN_EXPIRE);
+
+        // refreshToken 은 DB에 저장
+        LoginCredential loginCredential = loginCredentialRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("존재 하지 않는 회원입니다."));
+        loginCredential.setRefreshToken(refreshToken);
+        loginCredentialRepository.save(loginCredential);
+        return TokenResponseDto.from(refreshToken,accessToken);
     }
 }
