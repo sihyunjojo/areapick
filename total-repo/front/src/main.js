c780d17e53f251709ebb3cde8d22707d809380e3
박동현 <jinee9523@gmail.com>
2024-09-19 11:44:45 +0900

    Chore: pinia, bootstrap 추가

--- total-repo/front/src/main.js
+++ total-repo/front/src/main.js
@@ -1,8 +1,14 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
+// pinia 설정
+const pinia = createPinia();
 
+app.use(pinia);
 app.use(router);
 app.mount('#app');
