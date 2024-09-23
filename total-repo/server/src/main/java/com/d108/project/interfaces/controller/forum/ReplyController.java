271a3dd4e8e354bee4ec9cf9cdf07f099c7e7dca
박동현 <jinee9523@gmail.com>
2024-09-23 10:13:41 +0900

    Refactor: Reply Controller 방식 변경

--- total-repo/server/src/main/java/com/d108/project/interfaces/controller/forum/ReplyController.java
+++ total-repo/server/src/main/java/com/d108/project/interfaces/controller/forum/ReplyController.java
@@ -4,6 +4,7 @@ import com.d108.project.domain.forum.reply.ReplyService;
 import com.d108.project.domain.forum.reply.dto.ReplyByPostIdResponseDto;
 import com.d108.project.domain.forum.reply.dto.ReplyCreateDto;
 import com.d108.project.domain.forum.reply.dto.ReplyUpdateDto;
+import com.d108.project.domain.member.entity.Member;
 import com.d108.project.interfaces.api.forum.ReplyApi;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -21,10 +22,13 @@ public class ReplyController implements ReplyApi {
     private final ReplyService replyService;
 
 
-    @Operation(summary = "댓글 작성", description = "postId는 요청 주소를 통해서 보낸다.")
+    @Operation(summary = "댓글 작성", description =
+                    "<p>시큐리티에서 인증 정보를 받아옴</p>" +
+                    "<p>로그인 한 유저인지 확인해야함</p>"
+    )
     @Override
-    public ResponseEntity<Void> createReply(Long postId, ReplyCreateDto replyCreateDto) {
-        replyService.createReply(postId, replyCreateDto);
+    public ResponseEntity<Void> createReply(Member member, Long postId, ReplyCreateDto replyCreateDto) {
+        replyService.createReply(member, postId, replyCreateDto);
         return ResponseEntity.ok().build();
     }
 
@@ -36,18 +40,23 @@ public class ReplyController implements ReplyApi {
 
     @Operation(summary = "댓글 수정", description =
                     "<p>시큐리티에서 인증 정보를 받아옴</p>" +
-                    "<p>로그인 한 유저인지 확인해야함</p>"
+                    "<p>로그인 한 유저인지 확인해야함</p>" +
+                    "<p>postId는 사용하지 않음</p>"
     )
     @Override
-    public ResponseEntity<Void> updateReply(Long postId, Long replyId, Long memberId, ReplyUpdateDto replyUpdateDto) {
-        replyService.updateReply(replyId, memberId, replyUpdateDto);
+    public ResponseEntity<Void> updateReply(Member member, Long postId, Long replyId, ReplyUpdateDto replyUpdateDto) {
+        replyService.updateReply(member, replyId, replyUpdateDto);
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "댓글 삭제", description = "postId, replyId는 요청 주소를 통해서, memberId는 파라미터를 통해서 보낸다.")
+    @Operation(summary = "댓글 삭제", description =
+                    "<p>시큐리티에서 인증 정보를 받아옴</p>" +
+                    "<p>로그인 한 유저인지 확인해야함</p>" +
+                    "<p>postId는 사용하지 않음</p>"
+    )
     @Override
-    public ResponseEntity<Void> deleteReply(Long postId, Long replyId, Long memberId) {
-        replyService.deleteReply(replyId, memberId);
+    public ResponseEntity<Void> deleteReply(Member member, Long postId, Long replyId) {
+        replyService.deleteReply(member, replyId);
         return ResponseEntity.noContent().build();
     }
 }
