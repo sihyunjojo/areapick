68f22add7dfce03b875d3f8d49f5129e100ddc11
조시현 <si4018@naver.com>
2024-09-25 14:34:18 +0900

    fix: jpql 에러 해결

--- total-repo/server/src/main/java/com/d108/project/domain/area/analytic/repository/PopulationHistoryRepository.java
+++ total-repo/server/src/main/java/com/d108/project/domain/area/analytic/repository/PopulationHistoryRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface PopulationHistoryRepository extends JpaRepository<PopulationHistory, Long> {
 
     @Query("SELECT ph.populations20231, ph.populations20232, ph.populations20233, ph.populations20234, ph.populations20241 " +
-            "FROM PopulationHistory ph WHERE ph.area = :areaId")
+            "FROM PopulationHistory ph WHERE ph.area.id = :areaId")
     List<Long[]> getPopulationHistoryByAreaId(@Param("areaId") Long areaId);
 }
 
