ed1da94efb74d24891ed5eec01a4945b83ab5c85
박동현 <jinee9523@gmail.com>
2024-09-25 10:30:43 +0900

    Fix: merge 오류 수정

--- total-repo/server/src/main/java/com/d108/project/domain/forum/reply/service/ReplyService.java
+++ total-repo/server/src/main/java/com/d108/project/domain/forum/reply/service/ReplyService.java
@@ -1,22 +1,23 @@
-package com.d108.project.domain.forum.reply;
+package com.d108.project.domain.forum.reply.service;
 
 
 import com.d108.project.domain.forum.reply.dto.ReplyByMemberIdResponseDto;
 import com.d108.project.domain.forum.reply.dto.ReplyByPostIdResponseDto;
 import com.d108.project.domain.forum.reply.dto.ReplyCreateDto;
 import com.d108.project.domain.forum.reply.dto.ReplyUpdateDto;
+import com.d108.project.domain.member.entity.Member;
 
 import java.util.List;
 
 public interface ReplyService {
 
-    void createReply(Long postId, ReplyCreateDto replyCreateDto);
+    void createReply(Member member, Long postId, ReplyCreateDto replyCreateDto);
 
     List<ReplyByPostIdResponseDto> getAllReplyByPostId(Long postId);
 
     List<ReplyByMemberIdResponseDto> getAllReplyByMemberId(Long memberId);
 
-    void updateReply(Long replyId, Long memberId, ReplyUpdateDto replyUpdateDto);
+    void updateReply(Member member, Long replyId, ReplyUpdateDto replyUpdateDto);
 
-    void deleteReply(Long replyId, Long memberId);
+    void deleteReply(Member member, Long replyId);
 }
