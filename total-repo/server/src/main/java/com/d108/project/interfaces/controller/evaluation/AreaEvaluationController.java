02e7e31b5b26845d0adc6b7f956b320a1efe312b
조시현 <si4018@naver.com>
2024-09-26 12:29:25 +0900

    refactor: 타입 반환하는 api 추가

--- total-repo/server/src/main/java/com/d108/project/interfaces/controller/evaluation/AreaEvaluationController.java
+++ total-repo/server/src/main/java/com/d108/project/interfaces/controller/evaluation/AreaEvaluationController.java
@@ -1,7 +1,11 @@
 package com.d108.project.interfaces.controller.evaluation;
 
 import com.d108.project.domain.evaluation.areaEvaluation.dto.AreaEvaluationDto;
+import com.d108.project.domain.evaluation.areaEvaluation.dto.AreaEvaluationTypeListDto;
 import com.d108.project.domain.evaluation.areaEvaluation.service.AreaEvaluationService;
+import com.d108.project.interfaces.api.evaluation.AreaEvaluationAPI;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +13,16 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name="상권 평가")
 @RestController
 @RequestMapping("/api/area-evaluation")
 @RequiredArgsConstructor
-public class AreaEvaluationController {
+public class AreaEvaluationController implements AreaEvaluationAPI {
 
     private final AreaEvaluationService areaEvaluationService;
 
     // 평가 생성 (POST)
+    @Operation(summary = "[Member] 상권의 평가 생성 ", description = "")
     @PostMapping
     public ResponseEntity<AreaEvaluationDto> createEvaluation(@RequestBody AreaEvaluationDto dto) {
         AreaEvaluationDto createdEvaluation = areaEvaluationService.createEvaluation(dto);
@@ -24,6 +30,7 @@ public class AreaEvaluationController {
     }
 
     // 평가 수정 (PUT)
+    @Operation(summary = "[Member] 상권의 평가 수정 ", description = "")
     @PutMapping("/{id}")
     public ResponseEntity<AreaEvaluationDto> updateEvaluation(@PathVariable Long id, @RequestBody AreaEvaluationDto dto) {
         AreaEvaluationDto updatedEvaluation = areaEvaluationService.updateEvaluation(id, dto);
@@ -31,6 +38,7 @@ public class AreaEvaluationController {
     }
 
     // 평가 삭제 (DELETE)
+    @Operation(summary = "[Member] 상권의 평가 삭제 ", description = "")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteEvaluation(@PathVariable Long id) {
         areaEvaluationService.deleteEvaluation(id);
@@ -38,10 +46,19 @@ public class AreaEvaluationController {
     }
 
     // 특정 상권의 평가 조회 (GET)
+    @Operation(summary = "[ALL] 특정 상권의 평가 조회", description = "")
     @GetMapping("/area/{areaId}")
     public ResponseEntity<List<AreaEvaluationDto>> getEvaluationsByArea(@PathVariable Long areaId) {
         List<AreaEvaluationDto> evaluations = areaEvaluationService.getEvaluationsByArea(areaId);
         return ResponseEntity.ok(evaluations);
+    }
+
+
+    @Operation(summary = "[ALL] 상권 평가 타입 리스트 조회", description = "분위기, 유동인구 연령대, 유동인구 복잡 정도, 주변 물가 반환 ")
+    @Override
+    public ResponseEntity<AreaEvaluationTypeListDto> getEvaluationsByAreaTypeList() {
+        AreaEvaluationTypeListDto evaluationTypes = areaEvaluationService.getEvaluationsByAreaTypeList();
+        return ResponseEntity.ok(evaluationTypes);
     }
 }
 
