111731a3a8e77f3d8f7086382ce7ce9dfa52aaf7
박동현 <jinee9523@gmail.com>
2024-09-30 10:35:36 +0900

    Feat: 유저 정보 변경 로직 추가

--- total-repo/server/src/main/java/com/d108/project/domain/member/service/MemberService.java
+++ total-repo/server/src/main/java/com/d108/project/domain/member/service/MemberService.java
@@ -1,7 +1,9 @@
 package com.d108.project.domain.member.service;
 
+import com.d108.project.cache.redisEmail.dto.EmailAuthCheckDto;
 import com.d108.project.config.util.token.dto.TokenResponseDto;
 import com.d108.project.domain.member.dto.MemberLoginDto;
+import com.d108.project.domain.member.dto.MemberPasswordChangeDto;
 import com.d108.project.domain.member.dto.MemberRegisterDto;
 import com.d108.project.domain.member.dto.MemberResponseDto;
 import com.d108.project.domain.member.entity.Member;
@@ -20,9 +22,16 @@ public interface MemberService {
 
     MemberResponseDto getMyInfo(Member member);
 
+
     boolean isUsernameDuplicated(String username);
 
     boolean isNicknameDuplicated(String nickname);
 
     boolean isEmailDuplicated(String email);
+
+    void changeEmail(Member member, EmailAuthCheckDto emailAuthCheckDto);
+
+    void changePassword(Member member, MemberPasswordChangeDto memberPasswordChangeDto);
+
+    void changeNickname(Member member, String nickname);
 }
