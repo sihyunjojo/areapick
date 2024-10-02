844bdbbd67b800ac16fc4806f1c08cdc9658a8d9
조시현 <si4018@naver.com>
2024-10-02 13:43:13 +0900

    refactor: 관심 상권 변경

--- total-repo/server/src/main/java/com/d108/project/domain/area/dto/AreaListDto.java
+++ total-repo/server/src/main/java/com/d108/project/domain/area/dto/AreaListDto.java
@@ -1,12 +1,13 @@
 package com.d108.project.domain.area.dto;
 
 import com.d108.project.domain.area.entity.Area;
+import com.d108.project.domain.favorite.favoriteArea.entity.FavoriteArea;
 
 import java.util.List;
 
 public record AreaListDto(List<AreaDto> areaList) {
-    public static AreaListDto createAreaListDto(List<Area> areas) {
-        return new AreaListDto(areas.stream().
+    public static AreaListDto createAreaListDto(List<FavoriteArea> favoriteAreas) {
+        return new AreaListDto(favoriteAreas.stream().
                 map(AreaDto::to)
                 .toList()
         );
