6e75b91ec8b6a0682f0c8bb0381517ca890678f5
정진규 <wlsrb6905@naver.com>
2024-10-08 15:55:04 +0900

    Fix : 프랜차이즈 비용 분석 필드 입력 필수 추가

--- total-repo/front/src/views/franchise/FranchiseFee.vue
+++ total-repo/front/src/views/franchise/FranchiseFee.vue
@@ -91,7 +91,7 @@
                 <h3 class="mb-4"><span class="text-primary">매장 크기</span>를 선택해 주세요.</h3>
               <div class="row mb-4">
                 <div class="col-6">
-                  <div class="card" :class="{ 'border-primary': storeSize === 'small' }" @click="storeSize = 'small'">
+                  <div class="card" :class="{ 'border-primary': storeSize == 'small' }" @click="storeSize = 'small'">
                     <div class="card-body text-center">
                       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-shop" viewBox="0 0 16 16">
                         <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5M4 15h3v-5H4zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zm3 0h-2v3h2z"/>
@@ -102,7 +102,7 @@
                   </div>
                 </div>
                 <div class="col-6">
-                  <div class="card" :class="{ 'border-primary': storeSize === 'large' }" @click="storeSize = 'large'">
+                  <div class="card" :class="{ 'border-primary': storeSize == 'large' }" @click="storeSize = 'large'">
                     <div class="card-body text-center">
                       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-building" viewBox="0 0 16 16">
                         <path d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5z"/>
@@ -172,13 +172,12 @@ const showCategoryDropdown = ref(false)
 const showFranchiseDropdown = ref(false)
 const currentStep = ref(1)
 const location = ref('')
-const category = ref('')
-const franchise = ref('')
 const storeSize = ref('')
 const selectedFloor = ref('')
 const gu = ref('')
 const dong = ref('')
-
+const category = ref('')
+const franchise = ref('')
 
 const getGuInfos = () => {
   getGu(
@@ -270,6 +269,10 @@ const hideFranchiseDropdown = () => {
 }
 
 const nextStep = () => {
+  if(gu.value == '' || dong.value == '' || category.value=='' || franchise.value == '') {
+    window.alert("모든 선택지를 선택해주세요!")
+    return;
+  }
   currentStep.value++
 }
 
@@ -278,6 +281,10 @@ const prevStep = () => {
 }
 
 const showCostBreakdown = () => {
+  if(storeSize.value == '' || selectedFloor.value == '') {
+    window.alert("모든 선택지를 선택해주세요!")
+    return
+  }
   let size = storeSize.value == 'small' ? 10 : 20;
   myFranchise.value.storeSize = size
   let fee = selectedFloor.value == 'other' ? false : true;
