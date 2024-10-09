8682ab1657267c5c76d530b8318218a8793e3cd4
sihyunjojo <si4018@naver.com>
2024-10-09 15:31:38 +0900

    refactor: url 수정

--- total-repo/server/src/main/java/com/d108/project/interfaces/api/area/AreaInfoApi.java
+++ total-repo/server/src/main/java/com/d108/project/interfaces/api/area/AreaInfoApi.java
@@ -3,6 +3,7 @@ package com.d108.project.interfaces.api.area;
 import com.d108.project.domain.area.dto.AreaResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +13,7 @@ public interface AreaInfoApi {
     @GetMapping()
     ResponseEntity<AreaResponseDto> getAreaById(@RequestParam("areaId") Long areaId);
 
-    @GetMapping("/name")
-    public ResponseEntity<Long> getAreaID(@RequestParam String areaName);
+    @GetMapping("/name/{areaName}")
+    public ResponseEntity<Long> getAreaID(@PathVariable String areaName);
 
 }
