18c72a2ac3fdd5817a47ec498dc5918633d88251
sihyunjojo <si4018@naver.com>
2024-10-13 00:38:15 +0900

    feat: spring 기본 설정 파일 추가

---
+++ alert/src/main/java/com/joyride/alert/AlertApplication.java
@@ -0,0 +1,15 @@
+package com.joyride.alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertApplication.class, args);
+	}
+
+}
