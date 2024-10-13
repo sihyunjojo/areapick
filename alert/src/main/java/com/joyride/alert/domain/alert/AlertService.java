9b8265257bc3f679ec4e6b59bb818687f768c8bf
sihyunjojo <si4018@naver.com>
2024-10-13 18:18:37 +0900

    refactor: 카프카 토픽 내부 값 변경 및 전체 사용자에게 알림 보내기

--- alert/src/main/java/com/joyride/alert/domain/alert/AlertService.java
+++ alert/src/main/java/com/joyride/alert/domain/alert/AlertService.java
@@ -32,36 +32,54 @@ public class AlertService {
         alertRepository.saveAll(pendingNotifications);
     }
 
-    public void sendHotAreaAlert(Long memberId, String areaName) throws FirebaseMessagingException {
-        String title = "오늘 " + areaName + " 상권이 핫해요!";
-        String body =  "궁금하지 않으세요?";
-        sendAlert(memberId, title, body);
-    }
-
-    public void sendHotPostAlert(Long memberId, String postName, String boardName) throws FirebaseMessagingException{
-        String title = "오늘 " + boardName + " 게시판의 " + postName + " 글이 핫해요!";
-        String body =  "궁금하지 않으세요?";
-        sendAlert(memberId, title, body);
-    }
-
-    public void sendUpdateAlert(Long memberId, String type) throws FirebaseMessagingException {
-        String title = "새로운 " + type + " 정보가 업로드되었어요!";
-        String body = "새로운 " + type + " 정보가 업로드되었습니다. 확인해보세요!";
-        sendAlert(memberId, title, body);
-    }
-
-
     public void sendAlert(Long memberId, String title, String body) throws FirebaseMessagingException {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 id의 멤버가 존재하지 않습니다."));
 
         if (member.isOnline()) {
             String token = member.getFcmToken();
+            if (token.isEmpty()) {
+                saveOfflineAlert(member, title, body);
+            }
             notificationService.sendFcmNotification(token, title, body);
         } else {
             saveOfflineAlert(member, title, body);
         }
     }
+
+    public void sendHotAreaAlert(String areaName) throws FirebaseMessagingException {
+        String title = "오늘 " + areaName + " 상권이 핫해요!";
+        String body =  "궁금하지 않으세요?";
+
+        // 추후 배치로 처리
+        List<Member> allMember = memberRepository.findAll();
+        for (Member member : allMember) {
+            sendAlert(member.getId(), title, body);
+        }
+    }
+
+    public void sendHotPostAlert(String postName, String boardName) throws FirebaseMessagingException{
+        String title = "오늘 " + boardName + " 게시판의 " + postName + " 글이 핫해요!";
+        String body =  "궁금하지 않으세요?";
+
+        // 추후 배치로 처리
+        List<Member> allMember = memberRepository.findAll();
+        for (Member member : allMember) {
+            sendAlert(member.getId(), title, body);
+        }
+    }
+
+    public void sendUpdateAlert(String type) throws FirebaseMessagingException {
+        String title = "새로운 " + type + " 정보가 업로드되었어요!";
+        String body = "새로운 " + type + " 정보가 업로드되었습니다. 확인해보세요!";
+
+        // 추후 배치로 처리
+        List<Member> allMember = memberRepository.findAll();
+        for (Member member : allMember) {
+            sendAlert(member.getId(), title, body);
+        }
+    }
+
 
     private void saveOfflineAlert(Member member, String title, String body) {
         Alert pendingAlert = createPendingAlert(title, body, member);
