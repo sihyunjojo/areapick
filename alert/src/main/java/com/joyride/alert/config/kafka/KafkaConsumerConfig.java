3de26c60249ec5a17bf01951813dfb5529f58c5a
sihyunjojo <si4018@naver.com>
2024-10-15 02:37:56 +0900

    refactor: 가독성 향상

--- alert/src/main/java/com/joyride/alert/config/kafka/KafkaConsumerConfig.java
+++ alert/src/main/java/com/joyride/alert/config/kafka/KafkaConsumerConfig.java
@@ -52,8 +52,10 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyDeserializer);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializer);
 
+        // 수동 커밋을 사용하기 때문에 autoCommit을 false로 설정
         //오프셋 수동 관리
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, this.autoCommit);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, autoCommit);
+
         // 이 설정은 컨슈머가 어느 시점에서부터 메시지를 읽기 시작할지를 결정합니다.
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, this.earliest);
         // poll 요청을 보내고, 다음 poll 요청을 보내는데 까지의 최대 시간 설정
